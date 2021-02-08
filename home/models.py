264fe0b354f0d979603af542571d14b4c0201fb3
Neerajsamshette <neeraj.samshette@gmail.com>
2021-02-08 11:07:48 +0530

    Added Thank you page in contact form submission

--- home/models.py
+++ home/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Contact(models.Model):
-    id = models.AutoField(primary_key = True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100, default='')
     email = models.CharField(max_length=100, default='')
     phone_number = models.IntegerField(max_length=13, default='')
--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from .models import Contact
 
 # Create your views here.
 def home(request):
@@ -9,11 +10,15 @@ def about(request):
     return render(request, 'home/about.html')
 
 def contact(request):
+    CONTACT_SUBMITTED = False
     if request.method == 'POST':
         name = request.POST['name']
         email = request.POST['email']
         phone_number = request.POST['phoneNumber']
         issue = request.POST['issue']
-    CONTACT_SUBMITTED = True
+        contact = Contact(name = name, email=email, phone_number=phone_number, issue_message=issue)
+        contact.save()
+        CONTACT_SUBMITTED = True
+
 
     return render(request, 'home/contact.html', {"contact_status" : CONTACT_SUBMITTED})
