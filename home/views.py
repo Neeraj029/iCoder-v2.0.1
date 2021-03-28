bdb82309d21226db7b858c890e072ff3839f8903
Neerajsamshette <neeraj.samshette@gmail.com>
2021-03-28 17:21:22 +0530

    coding after final examination

--- home/views.py
+++ home/views.py
@@ -1,24 +1,29 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from .models import Contact
+from django.contrib import messages
+
 
 # Create your views here.
 def home(request):
     return render(request, 'home/index.html')
 
+
 def about(request):
     return render(request, 'home/about.html')
 
+
 def contact(request):
-    CONTACT_SUBMITTED = False
-    if request.method == 'POST':
+    CONTACT_SUBMITTED = False  # Initializing CONTACT_SUBMITTED with False
+    if request.method == 'POST':  # Ensuring to get name, email... only when there is POST method
         name = request.POST['name']
         email = request.POST['email']
         phone_number = request.POST['phoneNumber']
         issue = request.POST['issue']
-        contact = Contact(name = name, email=email, phone_number=phone_number, issue_message=issue)
-        contact.save()
-        CONTACT_SUBMITTED = True
+        # | Creating a object of Contact model |
+        contact = Contact(name=name, email=email, phone_number=phone_number, issue_message=issue)
+        contact.save()  # Saving information into database
+        CONTACT_SUBMITTED = True  # Changing CONTACT_SUBMITTED to True to get Thank you Message
 
-
-    return render(request, 'home/contact.html', {"contact_status" : CONTACT_SUBMITTED})
+    # Rendering contact.html with context contact_status : CONTACT_SUBMITTED
+    return render(request, 'home/contact.html', {"contact_status": CONTACT_SUBMITTED})
--- iCoder/settings.py
+++ iCoder/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+from django.contrib.messages import constants as messages
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,6 +120,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
